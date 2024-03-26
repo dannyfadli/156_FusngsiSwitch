ccf0cc054dd9f3992e1905570c97d70025787858
dannyfadli <danyfdly95@gmail.com>
2024-03-26 10:20:16 +0700

    menambahkan output luas dan memanggil kembali hasil yang sudah dihitung

--- fungsiSwitch/fungsiSwitch.cpp
+++ fungsiSwitch/fungsiSwitch.cpp
@@ -8,7 +8,7 @@ void prosedurLuas(int p, int l)
     luas = p * l;
 }
 
-int fungsikuas(int p, int l) 
+int fungsiLuas(int p, int l) 
 {
     return p * l;
 }
@@ -20,6 +20,11 @@ int main()
     cout << "Masukkan lebar: ";
     cin >> lebar;
 
-   
+    prosedurLuas(panjang, lebar);
+    cout << "Luas persegi panjang dengan prosedur" << luas << endl;
+    
+    cout << "Luas persegi panjang dengan fungsi" << fungsiLuas(panjang, lebar) << endl;
+
+    return 0;
 }
 
