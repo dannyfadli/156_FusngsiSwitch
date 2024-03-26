be2ed136d4e6ca4aec1c43d0a9beafd6e79571c6
dannyfadli <danyfdly95@gmail.com>
2024-03-26 11:02:52 +0700

    menambahkan program swiych case

--- tes_switch/tes_switch.cpp
+++ tes_switch/tes_switch.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
-int penjumlhan(int a, int b)
+int penjumlahan(int a, int b)
 {
     return a + b;
 }
@@ -33,6 +33,55 @@ void inputData()
 
 int main()
 {
-    
+    int pilihan;
+    do
+    {
+        system("CLS");
+        cout << "Menu kalkulator sederhana" << endl;
+        cout << "1.Penjumlahan" << endl;
+        cout << "2.pengurangan" << endl;
+        cout << "3.Perkalian" << endl;
+        cout << "4.Pembagian" << endl;
+        cout << "5.Keluar" << endl;
+        cout << "Masukkan pilhan" << " :";
+        cin >> pilihan;
+
+        switch (pilihan) 
+        {
+        case 1:
+            inputData();
+            cout << "n\Hasil penjumlahan: " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+
+        case 2:
+            inputData();
+            cout << "n\Hasil pengurangan: " << pengurangan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+
+        case 3:
+            inputData();
+            cout << "n\Hasil perkalian: " << perkalian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+
+        case 4:
+            inputData();
+            cout << "n\Hasil pembagian: " << pembagian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+
+        case 5:
+            break;
+
+        default:
+            cout << "Pilihan tidak valid" << endl;
+            system("pause");
+            break;
+        }
+
+    } while (pilihan != 5);         //!= adalah tidak sama dengan
+    return 0;
 }
 
